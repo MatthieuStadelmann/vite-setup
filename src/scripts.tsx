e3a58b67765a3cba4d7b59ff9cb789b93ce58db3
Matthieu Stadelmann <matthstadelmann@gmail.com>
2024-09-02 17:19:41 +0200

    Add typescript

---
+++ src/scripts.tsx
@@ -0,0 +1,16 @@
+import { createRoot } from "react-dom/client";
+import React from "react";
+
+function App() : JSX.Element {
+  return (
+    <div>
+      <h3>Hello There! What is your name?</h3>
+      <input></input>
+      <button onClick={() => alert("Hello")}>Say Hello</button>
+    </div>
+  );
+}
+
+const root = createRoot(document.getElementById("root"));
+
+root.render(<App />);
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,13 @@
-import legacy from '@vitejs/plugin-legacy'
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import legacy from '@vitejs/plugin-legacy'
+
 
 export default defineConfig({
   plugins: [
     legacy({
       targets: ['defaults', 'IE 11'],
     }),
+    react()
   ],
 })
